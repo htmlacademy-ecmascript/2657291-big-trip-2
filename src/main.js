d6524bacfe6cb0915b1da4a56aa93a109f581505
Александр Волков <AVolkov211@yandex.ru>
2026-05-27 23:26:56 +0300

    ДЗ 1.25. Время разбивать камни 2.0

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import BoardPresenter from './presenter/board-presenter';
 
-const presenter = new BoardPresenter({ container: document.body });
+const filterContainer = document.querySelector('.trip-controls__filters');
+const contentContainer = document.querySelector('.trip-events');
+
+const presenter = new BoardPresenter({ filterContainer, contentContainer });
 presenter.init();
+
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,29 +1,22 @@
 import { render } from '../render.js';
-import FormEditView from '../view/form-edit-view.js';
-import SortView from '../view/sort-view.js';
 import FilterView from '../view/filters-view.js';
-import PointView from '../view/route-point-view.js';
+import SortView from '../view/sort-view.js';
+import PointListView from '../view/point-list-view.js';
 
+const COUNT_POINT = 3;
 export default class BoardPresenter {
-  formEditView = new FormEditView();
-  sortComponent = new SortView();
   filterComponent = new FilterView();
+  sortComponent = new SortView();
+  pointList = new PointListView(COUNT_POINT);
 
-  constructor({ container }) {
-    this.container = container;
+  constructor({ filterContainer, contentContainer }) {
+    this.filterContainer = filterContainer;
+    this.contentContainer = contentContainer;
   }
 
   init() {
-    const filtersContainer = document.querySelector('.trip-controls__filters');
-    const sortContainer = document.querySelector('.trip-events__trip-sort');
-    const listContainer = document.querySelector('.trip-events__list');
-
-    render(this.filterComponent, filtersContainer);
-    render(this.sortComponent, sortContainer);
-    render(this.formEditView, listContainer);
-
-    for (let i = 0; i < 3; i++) {
-      render(new PointView(), listContainer);
-    }
+    render(this.filterComponent, this.filterContainer);
+    render(this.sortComponent, this.contentContainer);
+    render(this.pointList, this.contentContainer);
   }
 }
